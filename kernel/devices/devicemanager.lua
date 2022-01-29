a69f91ed40f2d873716f07f04d845ae104a5f195
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2022-01-29 22:38:03 +0100

    Initial commit

--- kernel/devices/devicemanager.lua
+++ kernel/devices/devicemanager.lua
@@ -1,4 +1,17 @@
+local kernel = nil
 local dm = {}
 dm.devices = {}
 
-dm.createDeviceDescriptor = nil
+dm.create = function(kernel_ref)
+    if type(kernel_ref) ~= "table" then
+        error("Bad kernel root reference")
+    end
+
+    if type(kernel_ref.protect) ~= "table" then
+        error("Dependencies not met, missing protect module")
+    end
+
+    kernel = kernel_ref
+end
+
+return dm
