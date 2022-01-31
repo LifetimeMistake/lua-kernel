fb4afd9cb2be440f3f789833abddd85c3efe697e
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2022-01-31 13:47:47 +0100

    Make progress on DeviceManager: * implement the char device descriptor * implement generic & char device registration

--- kernel/devices/devicemanager.lua
+++ kernel/devices/devicemanager.lua
@@ -2,13 +2,85 @@ local kernel = nil
 local dm = {}
 dm.devices = {}
 
+dm.createGenericDeviceStruct = function()
+    local device = {
+        majorNumber = nil,
+        minorNumber = nil,
+        name = nil
+    }
+
+    return device
+end
+
+dm.createCharDeviceStruct = function()
+    local charDevice = {
+        open = nil,
+        read = nil,
+        write = nil,
+        flush = nil,
+        release = nil,
+        ioctl = nil
+    }
+
+    local device = dm.createGenericDeviceStruct()
+    device.charOperations = charDevice
+
+    return device
+end
+
+dm.isDeviceRegistered = function(majorNumber, minorNumber)
+    if type(majorNumber) ~= "number" or type(minorNumber) ~= "number" then
+        error("Invalid arguments")
+    end
+
+    local deviceClass = dm.devices[majorNumber]
+    if not deviceClass then
+        return false
+    end
+
+    if not deviceClass[minorNumber] then
+        return false
+    end
+
+    return true
+end
+
+dm.createDevice = function(deviceStruct)
+    if type(deviceStruct) ~= "table" then
+        error("Invalid arguments")
+    end
+
+    if type(deviceStruct.majorNumber) ~= "number" or type(deviceStruct.minorNumber) ~= "number"
+    or type(deviceStruct.name) ~= "string" or #deviceStruct.name == 0 then
+        error("Bad device struct")
+    end
+
+    if dm.isDeviceRegistered(deviceStruct.majorNumber, deviceStruct.minorNumber) then
+        error("Device is already registered")
+    end
+
+    local deviceClass = dm.devices[deviceStruct.majorNumber]
+    if not deviceClass then
+        dm.devices[deviceStruct.majorNumber] = {}
+    end
+
+    deviceClass[deviceStruct.minorNumber] = deviceStruct
+end
+
+dm.createCharDevice = function(majorNumber, minorNumber, name)
+    local charDevice = dm.createCharDeviceStruct()
+    charDevice.majorNumber = majorNumber
+    charDevice.minorNumber = minorNumber
+    charDevice.name = name
+
+    dm.createDevice(charDevice)
+
+    return charDevice.charOperations
+end
+
 dm.create = function(kernel_ref)
     if type(kernel_ref) ~= "table" then
         error("Bad kernel root reference")
-    end
-
-    if type(kernel_ref.protect) ~= "table" then
-        error("Dependencies not met, missing protect module")
     end
 
     kernel = kernel_ref
